9a490d11b43c570f9a2046f5966e46e24e1b448c
Fernando Cavalcanti <comododk@gmail.com>
2024-11-09 14:22:00 -0300

    feat: add example endpoint and integrate AutomaticConceptsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AutomaticConceptsModule } from 'src/automatic-concepts/automatic-concepts.module';
 
 @Module({
-  imports: [],
+  imports: [AutomaticConceptsModule],
   controllers: [AppController],
   providers: [AppService],
 })
