7746ba4ecc7bedb3a34e40715028a8d7bd6133f0
Fernando Cavalcanti <comododk@gmail.com>
2024-12-09 22:16:42 -0300

    feat: remove AutomaticConceptsModule import from AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { MessagesModule } from 'src/messages/messages.module';
-import { AutomaticConceptsModule } from 'src/automatic-concepts/automatic-concepts.module';
 
 @Module({
   imports: [MessagesModule],
