f348a874fac18f451ce27fc2ffbb5fb876643ae3
Fernando Cavalcanti <comododk@gmail.com>
2025-06-07 18:12:58 -0300

    feat: add SimpleCacheInterceptor for caching responses in controllers

---
+++ src/common/interceptors/simple-cache.interceptor.ts
@@ -0,0 +1,29 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { of, tap } from 'rxjs';
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class SimpleCacheInterceptor implements NestInterceptor {
+  private readonly cache = new Map();
+
+  async intercept(context: ExecutionContext, next: CallHandler) {
+    console.log('SimpleCacheInterceptor: intercept called');
+    const request = context.switchToHttp().getRequest();
+    const url = request.url;
+
+    if (this.cache.has(url)) {
+      console.log(`Cache hit for URL: ${url}`);
+      return of(this.cache.get(url));
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    return next.handle().pipe(
+      tap((data) => {
+        this.cache.set(url, data);
+        console.log(`Cache set for URL: ${url}`);
+      }),
+    );
+  }
+}
