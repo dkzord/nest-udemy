60075ccfb82b80cb52281abd8b3b013f5b15eb32
Fernando <comododk@gmail.com>
2025-08-02 16:54:32 -0300

    feat: implement BcryptService for password hashing and create HashingServiceProtocol

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import { Global, Module } from '@nestjs/common';
+import { BcryptService } from './hashing/bcrypt.service';
+import { HashingServiceProtocol } from './hashing/hashing.service';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: HashingServiceProtocol,
+      useClass: BcryptService,
+    },
+  ],
+})
+export class AuthModule {}
